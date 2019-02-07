cbefe09663678945dba99cd6be87ccae57070a57
vtrev <baloyi.trev@gmail.com>
2019-02-07 16:30:00 +0200

    added full website

---
+++ js/script.js
@@ -0,0 +1,72 @@
+// initialize material and custom javascript
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    M.AutoInit();
+
+    var topSlider = document.querySelectorAll('#top-slider');
+    var carouselInstances = M.Carousel.init(topSlider, {
+        indicators: true,
+        fullWidth: true
+    });
+
+    var partnerCarousel = document.querySelectorAll('.partner-caurosel');
+    var partnerCarouselInstance = M.Carousel.init(partnerCarousel, {
+        indicators: false,
+        numVisible: 10,
+        dist: -20,
+        padding: 55
+    });
+
+
+
+    let slide = () => {
+        var carousel = document.getElementById('top-slider');
+        instance = M.Carousel.getInstance(carousel);
+        instance.next();
+        setTimeout(slide, 2500);
+    }
+    slide();
+});
+
+// Handlebars compilation for the partners caourosel
+
+let generatePartners = function (partners) {
+    var partnersElement = document.getElementById("partner-items");
+    var partnersTemplateSource = document.getElementById("partners-template").innerHTML;
+    var partnersTemplate = Handlebars.compile(partnersTemplateSource);
+    var partnersHTML = partnersTemplate(partners);
+    partnersElement.innerHTML = partnersHTML;
+};
+
+let partners = ['cput',
+    'cut',
+    'dut',
+    'forthare',
+    'mut',
+    'nmu',
+    'nwu',
+    'rhodes',
+    'sefako',
+    'sol',
+    'stellies',
+    'tut',
+    'uct',
+    'ufs',
+    'uj',
+    'ukzn',
+    'ul',
+    'ump',
+    'unisa',
+    'univen',
+    'unizulu',
+    'up',
+    'uwc',
+    'vut',
+    'wits',
+    'wsu'
+];
+
+generatePartners({
+    partners
+});
