ec020cce83d738267c5064e3c50e8989053b1fb1
vtrev <baloyi.trev@gmail.com>
2019-03-29 00:06:18 +0200

    added modals to all image

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.modal');
     var instances = M.Modal.init(elems, {
-        opacity:0.2
+        opacity:0.2,preventScrolling:false
     });
 
     var elems = document.querySelectorAll('.dropdown-trigger');
